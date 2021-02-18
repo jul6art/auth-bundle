16394c2840eb2ba2585fc70362e1d8d1d8a1a795
jul6art <geoffreyk42@gmail.com>
2021-02-18 23:19:01 +0100

    init

---
+++ SkeletonBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+// @TODO update the namespace
+namespace Jul6Art\AuthBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class AuthBundle.
+ *
+ * @TODO rename the class with YourBundleName
+ */
+class AuthBundle extends Bundle
+{
+}
